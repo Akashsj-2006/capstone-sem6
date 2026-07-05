a1528bf74b6e0f74dee913b5d87fe049323a5f51
Akashsj-2006 <akashsj2006@gmail.com>
2026-07-05 22:04:09 +0530

    Fix MongoDB Atlas connection

--- server/index.js
+++ server/index.js
@@ -10,10 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/classifier_db")
-.then(()=>console.log("MongoDB connected"))
-.catch(err=>console.log(err));
-
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const testSchema = new mongoose.Schema({
   name: String
